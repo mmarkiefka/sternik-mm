4d625e2a87060e55cab11c134eacd2496ff39fd0
mmarkiefka <marcel.markiefka@edu.uekat.pl>
2017-12-17 09:47:19 +0100

    drugie

---
+++ zad04/src/pl/sternik/mm/zadania/Main.java
@@ -0,0 +1,9 @@
+package pl.sternik.mm.zadania;
+import java.lang.*;
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("linjka 1");
+        System.out.println("linjka 2");
+    }
+}
